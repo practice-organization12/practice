5e569256beac45cd7cb8669cdcda7041c6e3e3c5
DESKTOP-MRU8EUO\804 <ganggwonlee@gmail.com>
2024-02-16 11:17:57 +0900

    first commit

---
+++ src/main/java/organization/githubpractice/GithubPracticeApplication.java
@@ -0,0 +1,14 @@
+package organization.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticeApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(GithubPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/organization/githubpractice/GithubPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package organization.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
