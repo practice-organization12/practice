3da83cd7a0eb9fc7f6f8337085b358d0826e9764
DESKTOP-MRU8EUO\804 <ganggwonlee@gmail.com>
2024-02-16 11:43:16 +0900

    hello world132

--- src/main/java/organization/githubpractice/GithubPracticeApplication.java
+++ src/main/java/organization/githubpractice/GithubPracticeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubPracticeApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello world2");
+        System.out.println("hello world22");
         SpringApplication.run(GithubPracticeApplication.class, args);
     }
 
